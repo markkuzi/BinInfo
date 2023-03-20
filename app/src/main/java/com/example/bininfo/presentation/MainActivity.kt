4f31fc1290d591606e46edf4f2e439e75c1e35fa
Markel Kuznetsov <markkuzi@yandex.ru>
2023-03-20 11:27:57 +0300

    Koin di created

--- app/src/main/java/com/example/bininfo/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bininfo/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.bininfo
+package com.example.bininfo.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bininfo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/bininfo/presentation/di/AppModules.kt
@@ -0,0 +1,47 @@
+package com.example.bininfo.presentation.di
+
+import androidx.room.Room
+import com.example.bininfo.data.localdb.BinDB
+import com.example.bininfo.data.repository.datasource.bin.BinApiDataSource
+import com.example.bininfo.data.repository.datasource.bin.BinDataSource
+import com.example.bininfo.data.repository.datasourceimpl.bin.BinApiDataSourceImpl
+import com.example.bininfo.data.repository.datasourceimpl.bin.BinDataSourceImpl
+import com.example.bininfo.data.repository.repository.BinRepository
+import com.example.bininfo.domain.repository.BinCall
+import com.example.bininfo.domain.usecase.BinUseCase
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val moduleBin = module {
+
+    single {
+        Room.databaseBuilder(androidContext(), BinDB::class.java,
+            "BinDB").build()
+    }
+
+    single { get<BinDB>().binDao }
+
+    single<BinDataSource> {
+        BinDataSourceImpl(
+            get()
+        )
+    }
+
+    single<BinApiDataSource> {
+        BinApiDataSourceImpl(
+            get()
+        )
+    }
+
+    single<BinCall> {
+        BinRepository(
+            get(),get()
+        )
+    }
+
+    single { BinUseCase(get()) }
+
+    viewModel { BinInfoViewModel(get()) }
+    viewModel { BinListViewModel(get()) }
+}
