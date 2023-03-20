2582e2b7cd1c7ab85306d881597ed355e8e3885a
Markel Kuznetsov <markkuzi@yandex.ru>
2023-03-20 10:18:59 +0300

    Data local database created

---
+++ app/src/main/java/com/example/bininfo/data/localdb/BinDB.kt
@@ -0,0 +1,11 @@
+package com.example.bininfo.data.localdb
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.bininfo.data.localdb.model.BinInfoDbModel
+
+@Database(entities = [BinInfoDbModel::class], version = 1)
+abstract class BinDB: RoomDatabase() {
+    abstract val binDao : BinDao
+
+}
---
+++ app/src/main/java/com/example/bininfo/data/localdb/model/BinInfoDbModel.kt
@@ -0,0 +1,45 @@
+package com.example.bininfo.data.localdb.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.bininfo.domain.entities.Status
+
+@Entity(tableName = "bin_info_list")
+data class BinInfoDbModel(
+    @PrimaryKey
+    @ColumnInfo(name = "binId")
+    val binId: String,
+    @ColumnInfo(name = "date")
+    val date: Long,
+    @ColumnInfo(name = "status")
+    val status: Status,
+    @ColumnInfo(name = "numberLength")
+    val numberLength: String?,
+    @ColumnInfo(name = "nuberLuhn")
+    val nuberLuhn: Boolean?,
+    @ColumnInfo(name = "scheme")
+    val scheme: String?,
+    @ColumnInfo(name = "type")
+    val type: String?,
+    @ColumnInfo(name = "brand")
+    val brand: String?,
+    @ColumnInfo(name = "prepaid")
+    val prepaid: Boolean?,
+    @ColumnInfo(name = "countryName")
+    val countryName: String?,
+    @ColumnInfo(name = "countryEmoji")
+    val countryEmoji: String?,
+    @ColumnInfo(name = "countryLatitude")
+    val countryLatitude: String?,
+    @ColumnInfo(name = "countryLongitude")
+    val countryLongitude: String?,
+    @ColumnInfo(name = "bankName")
+    val bankName: String?,
+    @ColumnInfo(name = "bankUrl")
+    val bankUrl: String?,
+    @ColumnInfo(name = "bankPhone")
+    val bankPhone: String?,
+    @ColumnInfo(name = "bankCity")
+    val bankCity: String?
+)
